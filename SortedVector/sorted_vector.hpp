9ef7064a25003a4ee95f9b8d6a785a50d34649f6
Anton <anton.lagosha.99@mail.ru>
2019-10-03 21:42:00 +0500

    perfomance of erase() and eraseAll() functions improved

--- SortedVector/sorted_vector.hpp
+++ SortedVector/sorted_vector.hpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <set>
+
 
 template<class ForwardIt, class T, class Compare = std::less<>>
 ForwardIt binary_find(ForwardIt first, ForwardIt last, const T& value, Compare comp = {})
@@ -16,92 +18,27 @@ ForwardIt binary_find(ForwardIt first, ForwardIt last, const T& value, Compare c
 }
 
 template<class Container, class T, class Compare = std::less<>>
-auto binary_find(Container& cont, const T& value, Compare comp = {})
+auto binary_find(Container& cont, const T& value, Compare comp = {}){ return binary_find(std::begin(cont), std::end(cont), value, comp); }
+
+template<class Container, class T, class Compare = std::less<>>
+auto binary_find(const Container& cont, const T& value, Compare comp = {}){ return binary_find(std::cbegin(cont), std::cend(cont), value, comp); }
+
+template<class ForwardIt, class T, class Compare = std::less<>>
+auto binary_find_range(ForwardIt first, ForwardIt last, const T& value, Compare comp = {})
 {
-	return binary_find(std::begin(cont), std::end(cont), value, comp);
+	first = std::lower_bound(first, last, value, comp);
+	if (first == last || comp(value, *first)) {
+		return std::make_pair(last, last);
+	}
+
+	return std::make_pair(first, std::upper_bound(first, last, value, comp));
 }
 
 template<class Container, class T, class Compare = std::less<>>
-auto binary_find(const Container& cont, const T& value, Compare comp = {})
-{
-	return binary_find(std::cbegin(cont), std::cend(cont), value, comp);
-}
+auto binary_find_range(Container& cont, const T& value, Compare comp = {}) { return binary_find_range(std::begin(cont), std::end(cont), value, comp); }
 
-template<class T, class Allocator = std::allocator<T>, class... Comparators>
-struct sorted_vector;
-
-template<class T, class Allocator, class Comparator>
-struct sorted_vector<T, Allocator, Comparator> {
-	using inner_container_type = std::vector<T, Allocator>;
-
-	using value_type = typename inner_container_type::value_type;
-	using allocator_type = typename inner_container_type::allocator_type;
-	using size_type = typename inner_container_type::size_type;
-	using difference_type = typename inner_container_type::difference_type;
-	using reference = typename inner_container_type::reference;
-	using const_reference = typename inner_container_type::const_reference;
-	using pointer = typename inner_container_type::pointer;
-	using const_pointer = typename inner_container_type::const_pointer;
-	using iterator = typename inner_container_type::iterator;
-	using const_iterator = typename inner_container_type::const_iterator;
-	using reverse_iterator = typename inner_container_type::reverse_iterator;
-	using const_reverse_iterator = typename inner_container_type::const_reverse_iterator;
-
-	auto insert(const T& val)
-	{
-		return elems_.insert(std::lower_bound(std::cbegin(elems_), std::cend(elems_), val, Comparator()), val);
-	}
-
-	auto erase(const T& val)
-	{
-		Comparator comp;
-		const auto it = std::lower_bound(std::cbegin(elems_), std::cend(elems_), val, comp);
-		return (it != std::cend(elems_) && !comp(*it, val)) ? elems_.erase(it, val) : std::cend(elems_);
-	}
-
-	void eraseAll(const T& val)
-	{
-		const auto itPair = std::equal_range(std::cbegin(elems_), std::cend(elems_), val, Comparator());
-		elems_.erase(itPair.first, itPair.second);
-	}
-
-	template<class It>
-	void assign(It first, It last) { std::for_each(first, last, [this](const T& val) { insert(val); }); }
-
-	template<class Cont>
-	void assign(const Cont& other) { assign(std::cbegin(other), std::cend(other)); }
-
-	void clear() { elems_.clear(); }
-	bool empty() const { return elems_.empty(); }
-
-	size_type size() const { return elems_.size(); }
-	size_type capacity() const { return elems_.capacity(); }
-
-	void reserve(size_type sz) { elems_.reserve(sz); }
-	void shrink_to_fit() { elems_.shrink_to_fit(); }
-
-	const T& at(size_type index) const { return elems_.at(index); }
-
-	auto cbegin() const { return elems_.cbegin(); }
-	auto cend() const { return elems_.cend(); }
-
-	auto begin() const { return cbegin(); }
-	auto end() const { return cend(); }
-
-	auto crbegin() const { return elems_.crbegin(); }
-	auto crend() const { return elems_.crend(); }
-
-	auto rbegin() const { return crbegin(); }
-	auto rend() const { return crend(); }
-
-	void swap(sorted_vector& other) { elems_.swap(other.elems_); }
-
-	bool operator==(const sorted_vector& other) const { return (elems_ == other.elems_); }
-	bool operator!=(const sorted_vector& other) const { return !(*this == other); }
-
-private:
-	inner_container_type elems_;
-};
+template<class Container, class T, class Compare = std::less<>>
+auto binary_find_range(const Container& cont, const T& value, Compare comp = {}) { return binary_find_range(std::cbegin(cont), std::cend(cont), value, comp); }
 
 
 template<class T, class Allocator, class... Comparators>
@@ -135,8 +72,10 @@ private: // local traits
 	template<class Comp>
 	static constexpr auto index_of_comp = index_of_v<Comp, Comparators...>;
 
+	static constexpr auto count_comparators = sizeof...(Comparators);
+
 public:
-	explicit sorted_vector() : sortedIndexes_{ sizeof...(Comparators) } {}
+	explicit sorted_vector() : sortedIndexes_{ count_comparators } {}
 
 	void insert(const T& val) { elems_.push_back(val); update_sorted(); }
 	void insert(T&& val) { elems_.push_back(std::move(val)); update_sorted(); }
@@ -144,15 +83,15 @@ public:
 	template<class... Args>
 	void emplace(Args&&... args) { elems_.emplace_back(std::forward<Args>(args)...); update_sorted(); }
 
-	bool erase(const T& val)
+	bool erase(const T& value)
 	{
-		const auto it = std::find(std::cbegin(elems_), std::cend(elems_), val);
-		if (it == std::cend(elems_)) {
+		const auto indexes_to_erase = find_elements_indexes(value);
+		if (indexes_to_erase.empty()) {
 			return false;
 		}
 
-		const auto erasedIndex = it - std::cbegin(elems_);
-		elems_.erase(it);
+		const auto erasedIndex = *(indexes_to_erase.begin());
+		elems_.erase(std::cbegin(elems_) + erasedIndex);
 		for (auto& indexes : sortedIndexes_) {
 			indexes.erase(std::find(std::cbegin(indexes), std::cend(indexes), erasedIndex));
 			for (auto& index : indexes) {
@@ -163,11 +102,27 @@ public:
 		}
 		return true;
 	}
-	bool eraseAll(const T& val)
+	bool eraseAll(const T& value)
 	{
-		if (!erase(val)) return false;
-		while (erase(val));
-		return true;
+		const auto indexes_to_erase = find_elements_indexes(value);
+		size_type shift = 0;
+		for (auto index_to_erase : indexes_to_erase) {
+			const auto currIndexToErase = index_to_erase - shift;
+			elems_.erase(std::cbegin(elems_) + currIndexToErase);
+
+			for (auto& sortedIndex : sortedIndexes_) {
+				sortedIndex.erase(std::find(std::cbegin(sortedIndex), std::cend(sortedIndex), currIndexToErase));
+				for (auto& index : sortedIndex) {
+					if (index > index_to_erase) {
+						--index;
+					}
+				}
+			}
+
+			++shift;
+		}
+
+		return (shift == 0);
 	}
 
 	void reserve(size_type space)
@@ -214,10 +169,10 @@ public:
 
 		const auto& currSorted = sortedIndexes_.at(index_of_comp<Comp>);
 
-		const auto foundIndexIt = std::lower_bound(std::cbegin(currSorted), std::cend(currSorted), value, compareByValues);
+		const auto foundIndexIt = binary_find(currSorted, value, compareByValues);
 		const auto lastIndexIt = std::cend(currSorted);
 
-		return (foundIndexIt != lastIndexIt && !comp(elems_.at(*foundIndexIt), value))
+		return (foundIndexIt != lastIndexIt)
 			? const_iterator<Comp>(elems_, foundIndexIt)
 			: const_iterator<Comp>(elems_, lastIndexIt);
 	}
@@ -316,33 +271,60 @@ public:
 	bool operator!=(const sorted_vector& other) const { return !(*this == other); }
 
 private:
+
+	template<class CompType>
+	struct ByValueComparatorAdaptor {
+		explicit ByValueComparatorAdaptor(const inner_container_type& valuesContext)
+			: pValuesContext_{ &valuesContext } {}
+
+		bool operator()(size_type left, size_type right) const { return comp_(pValuesContext_->at(left), pValuesContext_->at(right)); }
+		bool operator()(size_type left, const T& right) const { return comp_(pValuesContext_->at(left), right); }
+		bool operator()(const T& left, size_type right) const { return comp_(left, pValuesContext_->at(right)); }
+
+	private:
+		const inner_container_type* pValuesContext_ = nullptr;
+		CompType comp_;
+	};
+
 	template<class CurrComp>
-	bool for_every_of(size_type& currIndex)
+	bool for_every_of()
 	{
-		auto& currIndexes = sortedIndexes_.at(currIndex);
-
-		const auto compareByValues = [this](size_type left, size_type right) {
-			CurrComp comp;
-			return comp(elems_.at(left), elems_.at(right));
-		};
-
 		const auto currElemIndex = elems_.size() - 1;
-		currIndexes.insert(std::lower_bound(std::cbegin(currIndexes), std::cend(currIndexes), currElemIndex, compareByValues), currElemIndex);
-		++currIndex;
+		auto& currIndexes = sortedIndexes_.at(index_of_comp<CurrComp>);
+		currIndexes.insert(std::lower_bound(std::cbegin(currIndexes), std::cend(currIndexes), currElemIndex, ByValueComparatorAdaptor<CurrComp>(elems_)), currElemIndex);
+
 		return true;
 	}
 
 	void update_sorted() { insert_to_sorted(elems_.back()); }
-	void insert_to_sorted(const T& value)
+	void insert_to_sorted(const T& value) { bool do_this[]{ for_every_of<Comparators>()... }; }
+
+	template<class CurrComp>
+	auto find_by(const T& value) -> std::pair<typename std::vector<size_type>::iterator, typename std::vector<size_type>::iterator>
 	{
-		// "No matter that code is ugly, if it works well."
-		// (C) Jason Statham
-		size_type currCompIndex = 0;
-		bool do_this[]{ for_every_of<Comparators>(currCompIndex)... };
+		return binary_find_range(sortedIndexes_.at(index_of_comp<CurrComp>), value, ByValueComparatorAdaptor<CurrComp>(elems_));
+	}
+
+	auto find_elements_indexes(const T& value) -> std::set<size_type>
+	{
+		using iter_type = typename std::vector<size_type>::iterator;
+		using iters_pair = std::pair<iter_type, iter_type>;
+
+		iters_pair pairs_arr[] = { find_by<Comparators>(value)... };
+
+		std::set<size_type> result;
+		for (const auto currIters : pairs_arr) {
+			for (auto it = currIters.first; it != currIters.second; ++it) {
+				if (elems_.at(*it) == value) {
+					result.emplace(*it);
+				}
+			}
+		}
+
+		return result;
 	}
 
 private: // iterators
-
 	template<class CurrComp>
 	class const_iterator_impl : public std::iterator<std::random_access_iterator_tag, value_type, difference_type, const_pointer, const_reference> {
 		using base_type = std::iterator<std::random_access_iterator_tag, value_type, difference_type, const_pointer, const_reference>;
